ed00fb39100481ec6632db7c5ffcdad8d4521f77
kader009 <kadermollarasel000@gmail.com>
2023-02-24 22:43:43 +0600

    clean some unnecesarry code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import Display from './Display/Display';
+import Display from './Display/Display'; 
 
 
 function App() {
   const [name, setname] = useState('');
   const [data, setdata] = useState({});
+
+
 
 
   const fetchRequest = async () => {
@@ -30,31 +32,28 @@ function App() {
     fetch(`https://api.github.com/users/${name}`)
     .then(res => res.json())
     .then(find => {setdata(find)
-    
-      console.log(find);
     })
   }
 
   return (
     <div className="App">
-
       <h1 className='mt-3 mb-2'>Find Github user With Search</h1>
-
     <div className='container  w-50'>
-
 
     <form onSubmit={handleForm}>
     
-
     <input
     type={name}
     placeholder="Search github user ..."
     // value={name}
     onChange={onChange} className='mt-3'
     />
+
     </form>
 
     </div>
+
+    {/* pass data here */}
     <div>
       <Display data={data}  key={data.id}></Display>
     </div>
